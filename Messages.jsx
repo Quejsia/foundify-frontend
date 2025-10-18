4785a069479c92b7066b5c3ed046b098bc77165c
Quejsia <165565729+Quejsia@users.noreply.github.com>
2025-10-18 11:08:05 +0800

    Delete Messages.jsx

--- Messages.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-export default function Messages(){ return (
-  <div className="max-w-3xl mx-auto mt-6 p-4 bg-white rounded shadow">
-    <h2 className="font-semibold mb-2">Messages</h2>
-    <div className="text-sm">This page will show messages between claimers and owners.</div>
-  </div>
-) }
