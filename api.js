73865c18eca6fd4473babeae4fe2fd7f96f446e7
Quejsia <165565729+Quejsia@users.noreply.github.com>
2025-10-19 10:10:14 +0800

    Delete api.js

--- api.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios'
-const API = import.meta.env.VITE_API_URL || 'https://foundify-backend-12.onrender.com'
-const instance = axios.create({ baseURL: API })
-export default instance
