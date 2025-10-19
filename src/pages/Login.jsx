58a77f286cda072fa652f39720223d13d755db58
Quejsia <165565729+Quejsia@users.noreply.github.com>
2025-10-19 17:34:53 +0800

    Update and rename login.jsx to Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import api from '../../api'; // ✅ Correct path
+import React, { useState } from 'react';
+import api from '../../api';
 
 export default function Login({ onAuth }) {
   const [email, setEmail] = useState('');
